9cccb1e18b85eafed95f2f351f7b6167a412e7d1
Jing Mei <jingmei.may@gmail.com>
2020-03-23 13:16:13 +0800

    Update Header.js

--- src/gatsby-theme-carbon/components/Header.js
+++ src/gatsby-theme-carbon/components/Header.js
@@ -3,7 +3,7 @@ import Header from 'gatsby-theme-carbon/src/components/Header';
 
 const CustomHeader = props => (
   <Header {...props}>
-    Disease Progression Modeling
+    Disease Computational Modeling
   </Header>
 );
 
